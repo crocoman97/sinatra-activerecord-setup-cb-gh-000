3a3849d991c462c0992aa07fa333d704e84d0836
Rauof M <abdelrauof197@gmail.com>
2018-02-19 14:44:44 +0000

    automatically backed up by learn

--- db/migrate/20180219144057_create_dogs.rb
+++ db/migrate/20180219144057_create_dogs.rb
@@ -1,7 +1,12 @@
 class CreateDogs < ActiveRecord::Migration
   def up 
+    create_table :dogs do 
+      t.string :name 
+      t.string :breed 
+    end
   end
   
   def down
+    drop table :dogs
   end
 end
